b4a5d1a95281b2b6ff290d8bd35401ad6552cb81
jfabraxas <github@jfa.gmbh>
2022-06-25 09:00:07 +0200

    3 files modified, 5 added, 3 deleted

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,7 +1,11 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default (req, res) => {
+import { NextApiHandler } from '../../node_modules/next/types/index';
+
+const handler: NextApiHandler = (req, res) => {
   // Open Chrome DevTools to step through the debugger!
   // debugger;
   res.status(200).json({ name: 'Hello, world!' });
 };
+
+export default handler;
